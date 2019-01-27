eba3722829a538216cab4e9ff86ab004bc785882
benyou1969 <benyou.11@hotmail.com>
2019-01-27 23:07:35 +0100

    add some styles to card

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 @section('content')
   @foreach ($posts as $post)
-      <h1><a href="{{ $post->path() }}">{{ $post->title }}</a></h1>
+    <div class="card" style="margin-bottom: 20px">
+      <div class="card-header">
+        <a href="{{ $post->path() }}">{{ $post->title }}</a>
+        <small class="float-right">{{ $post->created_at->diffForHumans() }}</small>
+      </div>
+      <div class="card-body">
+        <blockquote class="blockquote mb-0">
+          <p>{{ $post->body }}</p>
+          <footer class="blockquote-footer">Created by <cite title="Source Title">Source Title</cite></footer>
+        </blockquote>
+      </div>
+    </div>
   @endforeach
 @endsection
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 @section('content')
-      <h1>{{ $post->title }}</h1>
-      <p>{{ $post->body }}</p>
+      <div class="card">
+            <div class="card-header">
+              <b>{{ $post->title }}</b>
+              <small class="float-right">{{ $post->created_at }}</small>
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="float-right blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+              <a href="{{ url("posts") }}" class="btn btn-primary">Go back</a>
+            </div>
+      </div>
 @endsection
