d3b53f100d97c1dc927f01deec0098cb082c1bcb
benyou1969 <benyou.11@hotmail.com>
2019-01-30 17:14:58 +0100

    Test an authenticated user can add comment

--- tests/Feature/CreateCommentsTest.php
+++ tests/Feature/CreateCommentsTest.php
@@ -14,12 +14,21 @@ class CreateCommentsTest extends TestCase
         $this->post = factory('App\Post')->create();
     }
 
-    public function test_an_authenticated_user_can_create_comment()
+    public function test_a_user_can_create_comment()
     {
         $this->post->addComment([
             'user_id' => 1,
             'body' => 'foo',
         ]);
         $this->assertCount(1, $this->post->comment);
+    }
+
+    public function test_an_authenticated_user_can_add_comment()
+    {
+        $this->be($user = factory('App\User')->create());
+        $comment = factory('App\Comment')->create(['post_id' => $this->post->id]);
+        $this->post($this->post->path().'/comments', $comment->toArray());
+        $this->get($this->post->path())
+             ->assertSee($comment->body);
     }
 }
