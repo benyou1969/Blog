4d7339775e0eb927a89606a61ab646ba2d39ef4c
benyou1969 <benyou.11@hotmail.com>
2019-01-30 14:06:46 +0100

    change UI and add FontAwesome

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,26 +1,37 @@
 @extends('layouts.app')
 @section('content')
-      <div class="card">
-            <div class="card-header">
-              <b>{{ $post->title }}</b>
-              <small class="float-right">{{ $post->created_at }}</small>
-            </div>
-            <div class="card-body">
-              <blockquote class="blockquote mb-0">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                <footer class="float-right blockquote-footer">Someone famous in <cite title="Source Title">{{ $post->creator->name}}</cite></footer>
-              </blockquote>
-              <a href="{{ url("posts") }}" class="btn btn-primary">Go back</a>
-            </div>
+  <div class="shadow-sm p-2 mb-5 bg-white rounded card border border-light">
+      <div class="card-header bg-info text-white">
+          <b>{{ $post->title }}</b>
+          <small class="float-right">{{ $post->created_at }}</small>
       </div>
-      <hr>
-      <div class="comments">
-      @foreach ($post->comment as $comment)
-        <div class="card">
-          <div class="card-body">
-           {{ $comment->body }}
+      <div class="card-body">
+          <blockquote class="blockquote mb-0">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="float-right blockquote-footer">Someone famous in <cite title="Source Title">{{ $post->creator->name}}</cite></footer>
+          </blockquote>
+          <a href="{{ url("posts") }}" class="btn btn-primary">Go back</a>
+      </div>
+  </div>
+  {{-- Comment --}}
+  <div class="row">
+    <div class="col-lg-8 col-md-7">
+      <div class="shadow-sm p-3 mb-5 bg-white rounded">
+        Comments <i class="far fa-comment-alt"></i>
+        @foreach ($post->comment as $comment)
+          <div class="shadow-sm mb-2 bg-white rounded card border border-light" style="margin-bottom:10px">
+            <div class="card-body">
+            {{ $comment->body }}
+            </div>
           </div>
-        </div>
-      @endforeach
+        @endforeach
      </div>
+    </div>
+ 
+    <div class="col-lg-4 col-md-5">
+      <div class="bg-white text-muted shadow-sm p-3 mb-5 bg-white rounded" style="padding: 10px 10px">
+        <p>  This post was published on <small>{{ $post->created_at }}</small>, by <b><a href="#">{{ $post->creator->name }}</a></b>, </p>
+      </div>
+    </div>
+ </div>
 @endsection
