240ef4cc9706e2f523e3488c5ea20a36c389f3e6
benyou1969 <benyou.11@hotmail.com>
2019-01-31 18:43:24 +0100

    Test

---
+++ tests/Feature/WritePostTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class WritePostTest extends TestCase
+{
+    use DatabaseMigrations;
+    public function test_an_authenticated_user_can_create_a_post()
+    {
+        $this->actingAs(factory('App\User')->create());
+        $post = factory('App\Post')->make();
+        $this->post('/posts', $post->toArray());
+        $this->get($post->path())
+             ->assertSee($post->title);
+        
+    }
+}
